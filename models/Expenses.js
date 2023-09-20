37bb3ff287eb54c9c0738517d2e895fa9ddaa63e
Hernan Tapia <soliditize@MacBook-Pro.local>
2023-09-19 22:57:15 -0700

    created mongoose expenses model

---
+++ models/Expenses.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose"
+
+const expensesSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    amount: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    
+}, 
+    { timestamps: true }
+);
+
+
+const Expenses = mongoose.models.expenses || mongoose.model 
+("expenses", expensesSchema)
+
+export default Expenses
