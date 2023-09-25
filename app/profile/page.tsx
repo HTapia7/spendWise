09d59e0ab0fc1c5ebee31be5320596c0866faff1
Hernan Tapia <soliditize@MacBook-Pro.local>
2023-09-24 23:09:12 -0700

    added html for form expense

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -32,6 +32,68 @@ export default function ProfilePage() {
             Logout
         </button>
 
+      <div className='flex flex-col items-center justified-start min-h-screen py-2 '>
+        <label htmlFor='title'>
+              Title: 
+            </label>
+            <input 
+              className='p-2 border border-black rounded-lg mb-4 '
+              id='title'
+              type='text'
+              // value={}
+              placeholder='Title'
+        />
+
+        <label htmlFor='amount'>
+              Amount: 
+        </label>
+            <input 
+              className='p-2 border border-black rounded-lg mb-4 '
+              id='amount'
+              type='number'
+              // value={}
+              placeholder='Amount'
+        />
+
+        <label htmlFor='date'>
+              Date: 
+        </label>
+            <input 
+              className='p-2 border border-black rounded-lg mb-4 '
+              id='date'
+              type='date'
+              // value={}
+        />
+
+        <label htmlFor='category'>
+              Category: 
+        </label>
+        <select id="category" name="category" className="p-2 border border-black rounded-lg mb-4 ">
+          <option value="fun">Fun</option>
+          <option value="bill">Bills</option>
+          <option value="gas">Gas</option>
+          <option value="food">Food</option>
+        </select>
+
+        <label htmlFor='description'>
+              Description: 
+        </label>
+            <input 
+              className='p-2 border border-black rounded-lg mb-4 '
+              id='description'
+              type='text'
+              // value={}
+              placeholder='Write a brief description'
+        />
+
+        <button 
+          className='p-2 border border-black rounded-lg mb-4 bg-green-500'
+          // onClick={onLogin}
+          >
+          Submit
+        </button>
+
+      </div>
     </div>
   )
 };
